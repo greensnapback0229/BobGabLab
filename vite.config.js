ef5e4c16cce4fa852cdb73c563c6e3da878614c3
zefa42 <youngoh07015@gmail.com>
2025-04-10 13:56:26 +0900

    refactor: proxy 주소 변경

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://server.meallab.site',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
